84339c2bf34ad7524fd16e4e737ae4c91c3e0aa5
Guillem <guillemb4b@gmail.com>
2024-06-03 13:14:52 +0200

    nivell 1 finished

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-"use strict";
-fetch("https://icanhazdadjoke.com/")
-    .then(res => console.log(res));
+mostrar();
+let randomJoke = '';
+function mostrar() {
+    document.getElementById('respuesta').innerHTML = '';
+    fetch("https://icanhazdadjoke.com/", {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+        .then(res => {
+        if (!res.ok) {
+            throw new Error('Network response was not ok ' + res.statusText);
+        }
+        return res.json();
+    })
+        .then(data => {
+        randomJoke = data.joke;
+        console.log(data.joke);
+        document.getElementById('randomJoke').innerHTML = randomJoke;
+    })
+        .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+}
+const reportJokes = [];
+function submitScore() {
+    let scores = document.getElementsByName('score');
+    let selectedScore;
+    for (const score of scores) {
+        if (score.checked) {
+            selectedScore = parseInt(score.value);
+            break;
+        }
+    }
+    const fechaActualISO = new Date().toISOString();
+    const report1 = {
+        joke: randomJoke,
+        score: selectedScore,
+        date: fechaActualISO
+    };
+    let existentJoke = reportJokes.find(element => element.joke == randomJoke);
+    if (!existentJoke) {
+        reportJokes.push(report1);
+        document.getElementById('respuesta').innerHTML = 'Gracies per la valoració';
+    }
+    else {
+        document.getElementById('respuesta').innerHTML = 'Ja has valorat aquest acudit';
+    }
+    console.log(reportJokes);
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,82 @@
-fetch("https://icanhazdadjoke.com/")
-    .then(res => console.log(res))
+
+mostrar()
+
+let randomJoke: string = ''
+
+function mostrar() {
+
+    document.getElementById('respuesta').innerHTML = ''
+
+    fetch("https://icanhazdadjoke.com/", {
+
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+
+        }
+        
+    })
 
 
+    .then(res => {
+        if (!res.ok) {
+        throw new Error('Network response was not ok ' + res.statusText);
+        }
+        return res.json();
+    })
+    
+    .then(data => {
+        randomJoke = data.joke
+        console.log(data.joke);
+        document.getElementById('randomJoke').innerHTML = randomJoke
+    })
+    
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
 
+}
+
+
+type report = {
+    joke: string
+    score: number
+    date: `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`;
+}
+
+const reportJokes = [];
+
+
+function submitScore(): void {
+
+    let scores = document.getElementsByName('score') as NodeListOf<HTMLInputElement>;
+    
+    let selectedScore: number | null;
+
+    for (const score of scores) {
+        if (score.checked) {
+            selectedScore = parseInt(score.value);
+            break;
+        }
+    }
+
+    const fechaActualISO = new Date().toISOString();
+
+    const report1: report = {
+        joke: randomJoke,
+        score: selectedScore,
+        date: fechaActualISO as `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`
+    }
+
+    let existentJoke = reportJokes.find(element => element.joke == randomJoke);
+
+    if (!existentJoke){
+        reportJokes.push(report1)
+        document.getElementById('respuesta').innerHTML = 'Gracies per la valoració'
+    } else {
+        document.getElementById('respuesta').innerHTML = 'Ja has valorat aquest acudit'
+    }
+    
+    
+    console.log(reportJokes)
+}
